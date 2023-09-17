13bf9c2989b806936f92fcdd11d3baa36dfc2de2
tiapez <59082782+tiapez@users.noreply.github.com>
2023-09-17 13:25:13 +0200

    major fix
    aggiunti i bottoni per ebay,cm e vinted
    aggiustamenti grafici
    fix immagini

--- src/app/Component/Card/card-list/modal-card-view/modal-card.component.ts
+++ src/app/Component/Card/card-list/modal-card-view/modal-card.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { Card } from 'src/app/Model/Card/card.model';
-import VanillaTilt from 'vanilla-tilt';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { CardActionService2 } from 'src/app/Service/Offline/CardAction/card-action.service';
 
 @Component({
   selector: 'app-modal-card',
@@ -12,16 +12,19 @@ export class ModalCardComponent {
 @Input() public modalCard! : Card;
 @Input() public modalSet! : string;
 
-constructor(public activeModal: NgbActiveModal) {
+constructor(public activeModal: NgbActiveModal, public cardService : CardActionService2) {
 }
-
-
 ngAfterViewInit(): void {
-  const elements:any = document.querySelector(".super-card");
-  VanillaTilt.init(elements);
-  if(this.modalCard.name.includes("Parallel") || this.modalCard.rarity.includes("Secret")){
-    const elements2:any = document.querySelector(".super-card-img");
-    VanillaTilt.init(elements2,{glare:true,max:0});
-  }
+  const element:any = document.querySelector(".modal-content");
+  element.classList.add("asdd");
 }
+
+// ngAfterViewInit(): void {
+//   const elements:any = document.querySelector(".super-card");
+//   // VanillaTilt.init(elements);
+//   if(this.modalCard.name.includes("Parallel") || this.modalCard.rarity.includes("Secret")){
+//     const elements2:any = document.querySelector(".super-card-img");
+//     // VanillaTilt.init(elements2,{glare:true,max:0});
+//   }
+// }
 }
--- src/app/Service/Offline/CardAction/card-action.service.ts
+++ src/app/Service/Offline/CardAction/card-action.service.ts
@@ -2,6 +2,10 @@ import { Injectable } from '@angular/core';
 import { CardDetails } from 'src/app/Model/CardDetails/card-details.model';
 import { ToastService } from '../../Implemented/Toast/toast.service';
 import { CardListService } from '../CardList/card-list.service';
+import { GlobalService } from '../../global.service';
+import { Set } from 'src/app/Model/Set/set.model';
+import { lastValueFrom } from 'rxjs/internal/lastValueFrom';
+import { Card } from 'src/app/Model/Card/card.model';
 
 
 @Injectable({
@@ -9,7 +13,7 @@ import { CardListService } from '../CardList/card-list.service';
 })
 export class CardActionService2 {
 
-  constructor(private toastService: ToastService, private cardService : CardListService) { }
+  constructor(private toastService: ToastService, private cardService : CardListService, private globalService : GlobalService) { }
 
 
   addClassic(card: CardDetails) {
@@ -24,4 +28,68 @@ export class CardActionService2 {
     this.cardService.saveUserCard(card.card.setId);
   }
 
+  async getCardMarketUrl(card : Card){
+    var url2 = "https://www.cardmarket.com/it/OnePiece/Products/Singles/Pillars-of-Strength/PortgasDAce-OP03-001-V1?language=1&minCondition=5";
+
+    var setList :  Set[] = await lastValueFrom(this.globalService.getSetList());
+    
+    var setName = "";
+    setList.forEach(set => {
+      if(set.id == card.setId){
+        setName =set.name.split(' ').join('-').split('.').join('');
+      }
+    });
+
+    var cardName = card.name.split('.').join('').split('"').join('').split(' ').join('-');
+    var cardNumber = card.number.split('_').join('-');
+    var cardSet = "-" + card.setId + "-";
+    if(card.setId != card.originalSetId)
+      cardSet = "-";
+    var url = "https://www.cardmarket.com/it/OnePiece/Products/Singles/"+setName+"/"+cardName+cardSet+cardNumber;
+
+    console.log(url);
+    window.open(url, "_blank");
+  }
+
+  async getEbaytUrl(card : Card){
+
+    var alt;
+    var number;
+    if(!/^[0-9]*$|.*V1$/.test(card.number)){
+      alt="alt";
+    }else{
+      alt="";
+    }
+    if(/^[A-Za-z]+/.test(card.number)){
+      number="";
+      alt="special"
+    }else{
+      number=card.number.substring(0,3);
+    }
+    var url = "https://www.ebay.com/sch/i.html?_nkw="+card.name+"+"+card.setId+" "+number+" "+alt;
+
+    console.log(url);
+    window.open(url, "_blank");
+  }
+
+  async getVintedtUrl(card : Card){
+
+    var alt;
+    var number;
+    if(!/^[0-9]*$|.*V1$/.test(card.number)){
+      alt="alt";
+    }else{
+      alt="";
+    }
+    if(/^[A-Za-z]+/.test(card.number)){
+      number="";
+      alt="special"
+    }else{
+      number=card.number.substring(0,3);
+    }
+    var url = "https://www.vinted.it/catalog?search_text="+card.name+"+"+card.setId+" "+number+" "+alt;
+
+    console.log(url);
+    window.open(url, "_blank");
+  }
 }
